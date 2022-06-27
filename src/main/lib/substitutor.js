0c5b08a0e37489ce99243bdd9d9bda2b758b1e03
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-27 10:30:21 +0100

    Undo merge from master

--- src/main/lib/substitutor.js
+++ src/main/lib/substitutor.js
@@ -1,13 +1,11 @@
-const ENV_VARS = ['ET_COS_URL', 'CCD_DEF_URL', 'CCD_DEF'];
+const ET_COS_URL = 'ET_COS_URL';
+const CCD_DEF_URL = 'CCD_DEF_URL';
 
 class Substitutor {
   static injectEnvironmentVariables (value) {
     Object.keys(process.env)
-      .filter(environmentVariableName => ENV_VARS.find(element => {
-          if (environmentVariableName.startsWith(element)) {
-            return true;
-          }
-      }))
+      .filter(environmentVariableName => environmentVariableName.startsWith(ET_COS_URL)
+        || environmentVariableName.startsWith(CCD_DEF_URL))
       .forEach(environmentVariableName => {
         const environmentVariableValue = process.env[environmentVariableName];
         value = value.replace(new RegExp('\\$\\{' + environmentVariableName + '\\}', 'g'), environmentVariableValue);
