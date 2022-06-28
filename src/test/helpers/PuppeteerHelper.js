f6fc8e12bf657fba20f0d17d5a40d1f8550dcf7c
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-06-28 14:42:05 +0100

    Add functional tests (#144)

---
+++ src/test/helpers/PuppeteerHelper.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+'use strict';
+
+const Helper = codecept_helper;
+const helperName = 'Puppeteer';
+const testForAccessibility = process.env.TESTS_FOR_ACCESSIBILITY || 'true';
+const { getAccessibilityTestResult } = require('../accessibility/runner');
+const { generateAccessibilityReport } = require('../reporter/customReporter');
+
+const { runAccessibility } = require('../accessibility/runner');
+
+class PuppeteerHelper extends Helper {
+  async runAccessibilityTest() {
+    if (!testForAccessibility) {
+      return;
+    }
+    const url = await this.helpers[helperName].grabCurrentUrl();
+    const { page } = await this.helpers[helperName];
+    await runAccessibility(url, page);
+  }
+  _finishTest() {
+    if (!testForAccessibility) {
+      return;
+    }
+    generateAccessibilityReport(getAccessibilityTestResult());
+  }
+
+  async waitForNavigationToComplete(locator) {
+    const page = this.helpers[helperName].page;
+    await page.waitForSelector(locator, {visible: true});
+    await page.click(locator);
+  }
+}
+
+module.exports = PuppeteerHelper;
